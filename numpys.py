e9e804f6e15c9e9e8e593638263e6ee7c408b346
erfan kheyrollahi <ekm507@gmail.com>
2019-08-12 21:39:43 +0430

    start learning numpy

---
+++ numpys.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.arange(-10, 10, 0.1)
+b = np.zeros([10, 10])
+c = np.sin(a)
+d = np.linspace(0, 10, 100)
+e = np.random.random()
+f = np.random.choice(a)
+print(f)
